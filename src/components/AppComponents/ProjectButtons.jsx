89a036c68ec30ec8c51b7488cd725a3831f52aa0
Jason <el.jsteer@gmail.com>
2024-05-06 19:00:48 +1000

    Button Styling

--- src/components/AppComponents/ProjectButtons.jsx
+++ src/components/AppComponents/ProjectButtons.jsx
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types";
 import Button from '@mui/material/Button';
 
-// import { useTheme } from "../../contexts/ThemeContext";
-// import { darkTheme, lightTheme } from "../../styles/Theme"; 
-// const { isDarkMode } = useTheme();
+import { useTheme } from "../../contexts/ThemeContext";
+import { darkTheme, lightTheme } from "../../styles/Theme"; 
+
+let darkThemeText = darkTheme.palette.text
+let lightThemeText = lightTheme.palette.text
 
 export default function ProjectButtons({text, href}) {
+  const { isDarkMode } = useTheme();
 
   ProjectButtons.propTypes = {
     text: PropTypes.element.isRequired,
@@ -14,8 +17,15 @@ export default function ProjectButtons({text, href}) {
 
   return (
     <a href={href} target="_blank" rel="noopener noreferrer">
-      <Button variant="outlined">
-        {text}
+      <Button
+        variant="outlined" 
+        sx={{
+          color: isDarkMode ? darkThemeText.primary : lightThemeText.primary,
+          border: `2px solid ${isDarkMode ? lightTheme.palette.primary.main : darkTheme.palette.primary.dark}`,
+          borderRadius: 0
+        }}
+          >
+          {text}
       </Button>
     </a>
   );
