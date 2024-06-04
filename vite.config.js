5eb8ce9fa9d6ae0cea58a8399eb7e54a8ffb49c1
Jason <el.jsteer@gmail.com>
2024-06-04 18:26:20 +1000

    update index.html path for main.jsx for pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolioDevJS/",
   optimizeDeps: {
     include: ['@mui/icons-material','@emotion/styled', '@mui/material/Unstable_Grid2']
   }
+
 })
