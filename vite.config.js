8721a580a9d3cd0a3d68e0788d5d65f982fbb280
Jason <el.jsteer@gmail.com>
2024-06-05 18:06:43 +1000

    update for successful deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolioDevJS/",
+  base: "./portfolioDevJS/",
   optimizeDeps: {
     include: ['@mui/icons-material','@emotion/styled', '@mui/material/Unstable_Grid2']
   }
