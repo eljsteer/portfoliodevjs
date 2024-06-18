61cc4e270e53c43c17828144ce719b648df13ebe
Jason <el.jsteer@gmail.com>
2024-06-18 18:34:16 +1000

    removed looping from swiper carousel to improve sync with project details

--- src/components/HomeProjectShowcase/ProjectShowcase.jsx
+++ src/components/HomeProjectShowcase/ProjectShowcase.jsx
@@ -48,7 +48,6 @@ export default function ProjectShowcase() {
             grabCursor={true}
             effect={'creative'}
             pagination={{ dynamicBullets: true }}
-            loop={true}
             creativeEffect={{
               prev: { shadow: true, translate: [0, 0, -400] },
               next: { translate: ['100%', 0, 0] },
