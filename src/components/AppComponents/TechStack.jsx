5efaefff10260ccde6e40595a5e1c7e7dc217bd9
Jason <el.jsteer@gmail.com>
2024-07-20 15:59:28 +1000

    update and added layout and overlap for icons and text:

--- src/components/AppComponents/TechStack.jsx
+++ src/components/AppComponents/TechStack.jsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Tooltip } from "@mui/material";
+import { Box, IconButton, Tooltip, Typography } from "@mui/material";
 import Fade from '@mui/material/Fade';
 
 import ResumeFile from "/assets/resume/Jason_Steer_Resumé.pdf"
@@ -11,11 +11,11 @@ import { SiBootstrap } from "react-icons/si";
 import { SiMui } from "react-icons/si";
 import { SiReact } from "react-icons/si";
 import { SiJavascript } from "react-icons/si";
-import { SiHandlebarsdotjs } from "react-icons/si";
+// import { SiHandlebarsdotjs } from "react-icons/si";
 import { TbApi } from "react-icons/tb";
 import { FaNodeJs } from "react-icons/fa";
 import { GrGraphQl } from "react-icons/gr";
-import { SiApollographql } from "react-icons/si";
+// import { SiApollographql } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
 import { SiMysql } from "react-icons/si";
 import { SiSequelize } from "react-icons/si";
@@ -31,21 +31,19 @@ export default function TechStack() {
   const { isDarkMode } = useTheme();
 
   const techStackArray = [
-    {Name: "HTML5", Icon: <SiHtml5 /> },
-    {Name: "CSS3", Icon: <SiCss3 /> },
-    {Name: "Bootstrap", Icon: <SiBootstrap /> },
-    {Name: "Material UI", Icon: <SiMui /> },
-    {Name: "Javascript", Icon: <SiJavascript /> },
-    {Name: "NodeJS", Icon: <FaNodeJs /> },
-    {Name: "ExpressJS", Icon: <SiExpress /> },
-    {Name: "ReactJS", Icon: <SiReact /> },
-    {Name: "MySQL", Icon: <SiMysql /> },
-    {Name: "Sequelize", Icon: <SiSequelize /> },
-    {Name: "MongoDB", Icon: <SiMongodb /> },
-    {Name: "GraphQL", Icon: <GrGraphQl /> },
-    {Name: "ApolloGraphQL" ,Icon: <SiApollographql /> },
-    {Name: "RESTfull APIs", Icon: <TbApi /> },
-    {Name: "HandlebarsJS", Icon: <SiHandlebarsdotjs /> }
+    {Id: 0, Name: "HTML5", Icon: <SiHtml5 className="icon" /> },
+    {Id: 1, Name: "CSS3", Icon: <SiCss3 className="icon" /> },
+    {Id: 2, Name: "Bootstrap", Icon: <SiBootstrap className="icon" /> },
+    {Id: 3, Name: "Material UI", Icon: <SiMui className="icon" /> },
+    {Id: 4, Name: "Javascript", Icon: <SiJavascript className="icon" /> },
+    {Id: 5, Name: "NodeJS", Icon: <FaNodeJs className="icon" /> },
+    {Id: 6, Name: "ExpressJS", Icon: <SiExpress className="icon" /> },
+    {Id: 7, Name: "ReactJS", Icon: <SiReact className="icon" /> },
+    {Id: 8, Name: "MySQL", Icon: <SiMysql className="icon" /> },
+    {Id: 9, Name: "Sequelize", Icon: <SiSequelize className="icon" /> },
+    {Id: 10, Name: "MongoDB", Icon: <SiMongodb className="icon" /> },
+    {Id: 11, Name: "GraphQL", Icon: <GrGraphQl className="icon" /> },
+    {Id: 12, Name: "RESTfull APIs", Icon: <TbApi className="icon" /> },
   ]
 
   return (
@@ -59,13 +57,15 @@ export default function TechStack() {
           </Tooltip>
         </a>
       </Box>
-      <ul>
+      <ul className="menu">
         {techStackArray.map((tech) => (
-          <li key={tech.Name}>  
+          <li key={tech.Id} style={{ "--i":tech.Id }} className="iconBox">  
             {tech.Icon}
+            <Typography className="iconText">{tech.Name}</Typography>
           </li>
         ))}
       </ul>
     </Box>
   );
 }
+
