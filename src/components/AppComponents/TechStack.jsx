a33eef8c562f6c34ae96d73e548f95e120fd9afc
Jason <el.jsteer@gmail.com>
2024-07-21 12:51:06 +1000

    udpated styling and colors for icon bacjgrounds

--- src/components/AppComponents/TechStack.jsx
+++ src/components/AppComponents/TechStack.jsx
@@ -7,11 +7,9 @@ import ResumeFile from "/assets/resume/Jason_Steer_Resumé.pdf"
 import { BsDownload } from "react-icons/bs";
 import { SiHtml5 } from "react-icons/si";
 import { SiCss3 } from "react-icons/si";
-import { SiBootstrap } from "react-icons/si";
 import { SiMui } from "react-icons/si";
 import { SiReact } from "react-icons/si";
 import { SiJavascript } from "react-icons/si";
-// import { SiHandlebarsdotjs } from "react-icons/si";
 import { TbApi } from "react-icons/tb";
 import { FaNodeJs } from "react-icons/fa";
 import { GrGraphQl } from "react-icons/gr";
@@ -31,19 +29,18 @@ export default function TechStack() {
   const { isDarkMode } = useTheme();
 
   const techStackArray = [
-    {Id: 0, Name: "HTML5", Icon: <SiHtml5 className="icon" /> },
-    {Id: 1, Name: "CSS3", Icon: <SiCss3 className="icon" /> },
-    {Id: 2, Name: "Bootstrap", Icon: <SiBootstrap className="icon" /> },
-    {Id: 3, Name: "Material UI", Icon: <SiMui className="icon" /> },
-    {Id: 4, Name: "Javascript", Icon: <SiJavascript className="icon" /> },
-    {Id: 5, Name: "NodeJS", Icon: <FaNodeJs className="icon" /> },
-    {Id: 6, Name: "ExpressJS", Icon: <SiExpress className="icon" /> },
-    {Id: 7, Name: "ReactJS", Icon: <SiReact className="icon" /> },
-    {Id: 8, Name: "MySQL", Icon: <SiMysql className="icon" /> },
-    {Id: 9, Name: "Sequelize", Icon: <SiSequelize className="icon" /> },
-    {Id: 10, Name: "MongoDB", Icon: <SiMongodb className="icon" /> },
-    {Id: 11, Name: "GraphQL", Icon: <GrGraphQl className="icon" /> },
-    {Id: 12, Name: "RESTfull APIs", Icon: <TbApi className="icon" /> },
+    {Id: 0, Name: "HTML5", Color: "#96D9D9", TextBg: "#2B7878", Icon: <SiHtml5 className="icon" />},
+    {Id: 1, Name: "CSS3", Color: "#E07A5F", TextBg: "#782C17", Icon: <SiCss3 className="icon" /> },
+    {Id: 2, Name: "Material UI", Color: "#84A98C", TextBg: "#436049", Icon: <SiMui className="icon" /> },
+    {Id: 3, Name: "Javascript", Color: "#1F656F", TextBg: "#123A40", Icon: <SiJavascript className="icon" /> },
+    {Id: 4, Name: "NodeJS", Color: "#83781B", TextBg: "#554E11", Icon: <FaNodeJs className="icon" /> },
+    {Id: 5, Name: "ExpressJS", Color: "#F1A208", TextBg: "#B27906", Icon: <SiExpress className="icon" /> },
+    {Id: 6, Name: "ReactJS", Color: "#96D9D9", TextBg: "#2B7878", Icon: <SiReact className="icon" /> },
+    {Id: 7, Name: "MySQL", Color: "#E07A5F", TextBg: "#782C17", Icon: <SiMysql className="icon" /> },
+    {Id: 8, Name: "Sequelize", Color: "#84A98C", TextBg: "#436049", Icon: <SiSequelize className="icon" /> },
+    {Id: 9, Name: "MongoDB", Color: "#1F656F", TextBg: "#123A40", Icon: <SiMongodb className="icon" /> },
+    {Id: 10, Name: "GraphQL", Color: "#83781B", TextBg: "#554E11", Icon: <GrGraphQl className="icon" /> },
+    {Id: 11, Name: "RESTfull APIs", Color: "#F1A208", TextBg: "#B27906", Icon: <TbApi className="icon" /> },
   ]
 
   return (
@@ -59,9 +56,9 @@ export default function TechStack() {
       </Box>
       <ul className="menu">
         {techStackArray.map((tech) => (
-          <li key={tech.Id} style={{ "--i":tech.Id }} className="iconBox">  
+          <li key={tech.Id} style={{ "--i":tech.Id, backgroundColor: tech.Color }} className="iconBox">  
             {tech.Icon}
-            <Typography className="iconText">{tech.Name}</Typography>
+            <Typography className="iconText" sx={{ backgroundColor: tech.TextBg }}> {tech.Name}</Typography>
           </li>
         ))}
       </ul>
