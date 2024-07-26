1e34555a45aa7bffd308264f8c5ecb36cd443da7
Jason <el.jsteer@gmail.com>
2024-07-26 19:06:46 +1000

    update to WIP Project Styling and also project data

--- src/components/AppComponents/WIPproject.jsx
+++ src/components/AppComponents/WIPproject.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
-import { Card, CardActionArea, CardMedia } from "@mui/material"
+import { Box, Button, Card, CardMedia, Stack, Typography } from "@mui/material"
 import { useState, useEffect } from 'react';
 
 import ProjectData from "../../utils/ProjectData.json"
+
+import "./styles/wipProject.css"
 
 const baseURL = import.meta.env.BASE_URL;
 
@@ -11,25 +13,36 @@ export default function WIPproject() {
   const [projectWIP, setProjectWIP] = useState(null)
 
   useEffect(() => {
-    ProjectData.forEach(project => {
-      if(project.projectWIP === true ) {
-        setProjectWIP(`${baseURL}${project.images[0].src}`)
+    const project = ProjectData.find(project => project.projectWIP === true);
+      if(project) {
+        setProjectWIP(project);
       }
-    });
   },[]);
-
+  
   WIPproject.propTypes = {
     project: PropTypes.object,
-  };  
+  }; 
+
+  if (!projectWIP) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Card elevation={5} sx={{ maxWidth: 420 }}>
-      <CardActionArea>
+      <Box id="WIPprojectContainer">
         <CardMedia
           component="img"
-          image={projectWIP}
+          image={`${baseURL}${projectWIP.images[0].src}`}
         />
-      </CardActionArea>
+        <Box id="imageOverlay">
+          <Typography variant="h5" id="imageCardTitle">{projectWIP.projectName}</Typography>
+          <Typography variant="body1" id="imageCardDescription">{projectWIP.description}</Typography>
+          <Stack direction="row" spacing={2}>
+            <Button className="WIPButtonStyle" href={projectWIP.deployedLink}>View Live</Button>
+            <Button className="WIPButtonStyle" href={projectWIP.github}>View Github</Button>
+          </Stack>
+        </Box>
+      </Box>
     </Card>
   );
 }
